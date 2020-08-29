37a8f629369e81582075019055621173e346fed5
Tonguc Gokceer <tongucgokceer@gmail.com>
2020-08-29 12:37:04 +0300

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
 
     def show
         @article=Article.find(params[:id])
+        @comment=Comment.new
+        @comment.article_id=@article_id #Due to the Rails' mass-assignment protection, manual assignment is needed !
     end
 
     def new
